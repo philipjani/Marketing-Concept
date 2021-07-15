95261ac68db55be323bccbe1eb079511112cc51f
Philip Jani <philipjani@yahoo.com>
2021-07-15 01:18:44 -0400

    Added Leads table to db and view

--- app.py
+++ app.py
@@ -22,10 +22,13 @@ class Lead(db.Model):
     city = db.Column(db.String(200), nullable=False)
     state = db.Column(db.String(200), nullable=False)
     zip_code = db.Column(db.String(200), nullable=False)
+    owner_occupied = db.Column(db.String, nullable=False)
+    property_type = db.Column(db.String, nullable=False)
+    mls_status = db.Column(db.String, nullable=False)
     phone_number = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     contacted = db.Column(db.Integer, default=0)
-    contact_time = db.Column(db.DateTime, nullable=False)
+    contact_time = db.Column(db.DateTime)
     template_sent = db.Column(db.String(200), nullable=False)
     response = db.Column(db.String(200), nullable=False)
     motivation_level = db.Column(db.String(200), nullable=False)
@@ -46,13 +49,23 @@ def index():
             df.columns = df.columns.str.strip()
             con = sqlite3.connect("test.db")
             #https://stackoverflow.com/questions/3548673/how-can-i-replace-or-strip-an-extension-from-a-filename-in-python
-            filename = os.path.splitext(uploaded_file.filename)
-            filename = filename[0]
-            df.to_sql(filename, con)
+            # filename = os.path.splitext(uploaded_file.filename)
+            # filename = filename[0]
+            # df.to_sql('filename', con)
+            df.to_sql('lead', con, if_exists='replace')
             con.close()
         return redirect(url_for('index'))
     else:
         return render_template('index.html')
+
+@app.route('/test')
+def test():
+    con = sqlite3.connect('test.db')
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM lead LIMIT 15;")
+    data = cur.fetchall()
+    return render_template('test.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
