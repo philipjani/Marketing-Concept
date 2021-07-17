c76bd6cb2cb3b4229c67878910473660bc992d99
Philip Jani <philipjani@yahoo.com>
2021-07-17 15:23:00 -0400

    added flask-migrate and templates route & model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sqlite3
 import pandas as pd
 import os
@@ -13,6 +14,8 @@ UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/files')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
+#https://flask-migrate.readthedocs.io/en/latest/
+migrate = Migrate(app, db)
 
 class Lead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +39,14 @@ class Lead(db.Model):
     def __repr__(self):
         return '<Lead %r>' % self.id
 
+class Template(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Template %r>' % self.id
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -51,7 +62,7 @@ def index():
             #https://stackoverflow.com/questions/3548673/how-can-i-replace-or-strip-an-extension-from-a-filename-in-python
             # filename = os.path.splitext(uploaded_file.filename)
             # filename = filename[0]
-            # df.to_sql('filename', con)
+            # df.to_sql(filename, con)
             df.to_sql('lead', con, if_exists='replace')
             con.close()
         return redirect(url_for('index'))
@@ -67,9 +78,35 @@ def leads():
     data = cur.fetchall()
     return render_template('leads.html', data=data)
 
-@app.route('/templates')
+@app.route('/templates', methods = ["POST", "GET"])
 def templates():
-    return render_template('templates.html')
+    if request.method == 'POST':
+        temp_name = request.form['name']
+        temp_message = request.form['message']
+        sms = Template(name=temp_name, message=temp_message)
+        try:
+            db.session.add(sms)
+            db.session.commit()
+            return redirect('/templates')
+        except:
+            return 'There was an issue adding your template'
+    else:
+        #https://stackoverflow.com/questions/2633218/how-can-i-select-all-rows-with-sqlalchemy/26217436
+        sms_templates = Template.query.all()
+        return render_template('templates.html', sms_templates=sms_templates)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sms_to_delete = Template.query.get_or_404(id)
+
+    try:
+        #https://flask-sqlalchemy.palletsprojects.com/en/2.x/queries/
+        db.session.delete(sms_to_delete)
+        db.session.commit()
+        return redirect('/templates')
+    except:
+        return 'There was an error in deleting the template.'
+
 
 @app.errorhandler(404)
 def page_not_found(error):
