895788ad5039a464f56879548b576913b70ac78f
3Ring <celestialswashbuckler@gmail.com>
2022-02-12 17:50:40 -0500

    bug

--- project/views/replies.py
+++ project/views/replies.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint, render_template
 from flask_login import login_required
 import pytz
@@ -6,33 +5,41 @@ from project.models import Phone_Number, TextReply, Lead
 
 replies = Blueprint("replies", __name__)
 
-@replies.route('/replies', methods=["GET", 'POST'])
+
+@replies.route("/replies", methods=["GET", "POST"])
 @login_required
 def main():
-    replies = TextReply.query.all()
-    for r in replies:
+    _replies = TextReply.query.all()
+    print(f"replies: {_replies}")
+    replies = []
+    for r in _replies:
         num = Phone_Number.query.get(r.phone_id)
         lead = Lead.query.get(num.lead_id)
-        r.additional["address"] = lead.address
-        r.additional["city"] = lead.city
-        r.additional["state"] = lead.state
-        r.additional["zip"] = lead.zip
-        r.additional["first_name"] = lead.first_name
-        r.additional["last_name"] = lead.last_name
-        r.additional["property_type"] = lead.property_type
-        r.additional["mls_status"] = lead.mls_status
-        r.additional["template_sent"] = lead.template_sent
-        r.additional["number"] = num.mobile_phone
-        r.additional["date"] = convert_date(r.contact_time)
-        r.additional["time"] = convert_time(r.contact_time)
+        replies.append(
+            {"address": lead.address,
+                "city": lead.city,
+                "state": lead.state,
+                "zip": lead.zip,
+                "first_name": lead.first_name,
+                "last_name": lead.last_name,
+                "property_type": lead.property_type,
+                "mls_status": lead.mls_status,
+                "template_sent": lead.template_sent,
+                "number": num.mobile_phone,
+                "date": convert_date(r.contact_time),
+                "time": convert_time(r.contact_time),
+            }
+        )
     return render_template("replies.html", replies=replies)
 
+
 def convert_date(time):
-    est = pytz.timezone('US/Eastern')
+    est = pytz.timezone("US/Eastern")
     fmt = "%m-%d-%Y"
     return time.astimezone(est).strftime(fmt)
-    
+
+
 def convert_time(time):
-    est = pytz.timezone('US/Eastern')
+    est = pytz.timezone("US/Eastern")
     fmt = "%I:%M %p"
     return time.astimezone(est).strftime(fmt)
